ff88e0d865de60d52b53695576a0bec04c670d7f
ShivajiBajpai98 <84443265+ShivajiBajpai98@users.noreply.github.com>
2022-05-31 11:19:11 +0530

    Initial commit

---
+++ src/main/java/base/CarBase.java
@@ -0,0 +1,66 @@
+package base;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CarBase {
+	
+	WebDriver driver;
+	
+	public CarBase(WebDriver driver) {
+		
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	@FindBy(xpath="//*[@id=\"root\"]/div[2]/div/div[2]/div/h1")
+	public WebElement carTitle;
+
+	/*public CarBase() {
+
+	}*/
+
+	public String getCarTitle() {
+		
+	
+		return carTitle.getText();
+		
+	}
+	
+	@FindBy(xpath = "//div/div/div/a/h3")
+	//@FindBy(tagName = "h3")
+	public List<WebElement> carName;
+
+	//@FindBy(xpath = "//div/div/div[1]/div[2]/span[1]")
+	@FindBy(xpath = "span[contains(text(),'Lakh')]")
+	public List<WebElement> carPrices;
+	
+	
+	
+	public void getCarNameAndPrices() {
+		
+		for(int i=0; i<carPrices.size(); i++) {
+			
+			
+			System.out.println(carName.get(i).getText()+"----car price is : "+carPrices.get(i).getText());
+			//System.out.println(carName.get(i).getText());
+			
+		}
+		
+		
+	}
+
+	/*public static void main(String[] args)
+	{
+
+		CarBase carBase = new CarBase();
+		carBase.getCarNameAndPrices();
+
+	}*/
+	
+}
